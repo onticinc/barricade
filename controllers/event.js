dd07e84f9552f1064aeef62c97ee48b98f80c411
onticinc <onticinc@gmail.com>
2021-12-08 22:40:17 -0700

    installed multer for uploading pictures

--- controllers/event.js
+++ controllers/event.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const { Event } = require('../models');
+
 
 /**
  * ============================
