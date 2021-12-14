859c958ca096b760b772a7d903debbea19b4b627
onticinc <onticinc@gmail.com>
2021-12-13 22:54:28 -0700

    debugging

--- controllers/event.js
+++ controllers/event.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer')
-const upload = multer({ dest: '../uploads' })
 const { Event } = require('../models');
 
 /**
